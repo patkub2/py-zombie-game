22fc39d2464656208d7d56a3e374053562b23819
patrykkubala <patrykkubala1999@gmail.com>
2022-04-29 21:29:03 +0200

    changed enemy into enemynormal and added a shooting enemy

--- main.py
+++ main.py
@@ -4,7 +4,9 @@ import pygame
 import random
 import math
 from Player import Player
-from Enemy import Enemy
+from EnemyNormal import EnemyNormal
+from EnemyNormal import EnemyNormal
+
 
 pygame.init()
 size    = (800, 600)
@@ -18,23 +20,22 @@ pygame.display.set_caption("Top Down")
 done = False
 hero = pygame.sprite.GroupSingle(Player(screen.get_size()))
 enemies = pygame.sprite.Group()
-lastEnemy = 0
+lastEnemyNormal = 0
 score = 0
 clock = pygame.time.Clock()
 
+#=========================== Damage calculation ============================
 def move_entities(hero, enemies, timeDelta):
     score = 0
     hero.sprite.move(screen.get_size(), timeDelta)
     for enemy in enemies:
         enemy.move(enemies, hero.sprite.rect.center, timeDelta)
-        enemy.shoot(hero.sprite.rect.center)
-    for proj in Enemy.projectiles:
-        proj.move(screen.get_size(), timeDelta)
-        if pygame.sprite.spritecollide(proj, hero, False):
-            proj.kill()
+        if pygame.sprite.spritecollide(enemy, hero, False):
+            enemy.kill()
             hero.sprite.health -= 1
             if hero.sprite.health <= 0:
                 hero.sprite.alive = False
+  
     for proj in Player.projectiles:
         proj.move(screen.get_size(), timeDelta)
         enemiesHit = pygame.sprite.spritecollide(proj, enemies, True)
@@ -43,15 +44,15 @@ def move_entities(hero, enemies, timeDelta):
             score += len(enemiesHit)
     return score
 
+#=========================== Rendering ============================
 def render_entities(hero, enemies):
     hero.sprite.render(screen)
     for proj in Player.projectiles:
         proj.render(screen)
-    for proj in Enemy.projectiles:
-        proj.render(screen)
     for enemy in enemies:
         enemy.render(screen)
     
+#=========================== Player moving ============================
 def process_keys(keys, hero):
     if keys[pygame.K_w]:
         hero.sprite.movementVector[1] -= 1
@@ -67,18 +68,19 @@ def process_keys(keys, hero):
         hero.sprite.equippedWeapon = hero.sprite.availableWeapons[1]
     if keys[pygame.K_3]:
         hero.sprite.equippedWeapon = hero.sprite.availableWeapons[2]
-        
+
+#=========================== Player shooting ============================     
 def process_mouse(mouse, hero):
     if mouse[0]:
         hero.sprite.shoot(pygame.mouse.get_pos())
 
-#GAME LOOP-================================================
+#=========================== Main game loop ============================  
 def game_loop():
     done = False
     player = Player(screen.get_size())
     hero = pygame.sprite.GroupSingle(Player(screen.get_size()))
     enemies = pygame.sprite.Group()
-    lastEnemy = pygame.time.get_ticks()
+    lastEnemyNormal = pygame.time.get_ticks()
     score = 0
     
     while hero.sprite.alive and not done:
@@ -97,21 +99,20 @@ def game_loop():
         process_keys(keys, hero)
         process_mouse(mouse, hero)
         hero.update()
-        pygame.draw.circle(screen, (255, 128, 0), [int(i) for i in player.pos], 3)
-        pygame.draw.rect(screen, (255, 128, 0), player.rect, 2)
         
-        # Enemy spawning process
-        if lastEnemy < currentTime - 200 and len(enemies) < 50:
+        
+        # EnemyNormal spawning process
+        if lastEnemyNormal < currentTime - 2000 and len(enemies) < 50:
             spawnSide = random.random()
             if spawnSide < 0.25:
-                enemies.add(Enemy((0, random.randint(0, size[1]))))
+                enemies.add(EnemyNormal((0, random.randint(0, size[1]))))
             elif spawnSide < 0.5:
-                enemies.add(Enemy((size[0], random.randint(0, size[1]))))
+                enemies.add(EnemyNormal((size[0], random.randint(0, size[1]))))
             elif spawnSide < 0.75:
-                enemies.add(Enemy((random.randint(0, size[0]), 0)))
+                enemies.add(EnemyNormal((random.randint(0, size[0]), 0)))
             else:
-                enemies.add(Enemy((random.randint(0, size[0]), size[1])))
-            lastEnemy = currentTime
+                enemies.add(EnemyNormal((random.randint(0, size[0]), size[1])))
+            lastEnemyNormal = currentTime
         
         score += move_entities(hero, enemies, clock.get_time()/17)
         render_entities(hero, enemies)
@@ -129,7 +130,7 @@ def game_loop():
         clock.tick(120)
 
 done = game_loop()
-#END GAME LOOP-================================================
+#=========================== End main game loop ============================  
 while not done:
     keys = pygame.key.get_pressed()
     mouse = pygame.mouse.get_pressed()
